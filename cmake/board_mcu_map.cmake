2a541d1896c70e6923524a15e2cfc56d08d502f4
Yaroslav Yasynytskyi <yaroslav.yasynytskyi@gmail.com>
2024-01-22 18:02:40 +0100

    build: backbone of cmake-based buildsystem
    Signed-off-by: Yaroslav Yasynytskyi <yaroslav.yasynytskyi@gmail.com>

---
+++ cmake/board_mcu_map.cmake
@@ -0,0 +1,11 @@
+include(${CMAKE_SOURCE_DIR}/cmake/map.cmake)
+
+function(board_mcu_map_insert board mcu)
+    microjava_map_insert(board_mcu ${board} ${mcu})
+endfunction()
+
+function(board_mcu_map_find destination board)
+    microjava_map_find(board_mcu ${destination} ${board})
+endfunction()
+
+board_mcu_map_insert(pico rp2040)
---
+++ cmake/null.c
@@ -0,0 +1,4 @@
+/**
+ * This empty file is needed to compile microjava target
+ * See https://stackoverflow.com/questions/34234446 for more info
+*/
